83be92c30970d7253affd2e41e0daa67380d8f1d
zanadoman <domanseven@gmail.com>
2025-04-21 19:51:11 +0200

    archetype reset function to prevent realloc in entity manager

--- src/ecs/entity_manager.rs
+++ src/ecs/entity_manager.rs
@@ -1,48 +1,51 @@
 use super::{Archetype, Entity};
 
 pub struct EntityManager {
-    archetypes: Vec<Option<Archetype>>,
-    destroyed: Vec<Entity>,
+    entities: Vec<(bool, Archetype)>,
+    destroyed: Vec<usize>,
 }
 
 impl EntityManager {
     #[must_use]
     pub const fn new() -> Self {
         Self {
-            archetypes: Vec::new(),
+            entities: Vec::new(),
             destroyed: Vec::new(),
         }
     }
 
     #[must_use]
     pub fn spawn(&mut self) -> Entity {
-        if let Some(entity) = self.destroyed.pop() {
-            self.archetypes[entity.id()] = Some(Archetype::new());
-            entity
+        if let Some(id) = self.destroyed.pop() {
+            self.entities[id].1.reset();
+            self.entities[id].0 = true;
+            Entity::new(id)
         } else {
-            let entity = Entity::new(self.archetypes.len());
-            self.archetypes.push(Some(Archetype::new()));
+            let entity = Entity::new(self.entities.len());
+            self.entities.push((true, Archetype::new()));
             entity
         }
     }
 
     #[must_use]
     pub fn archetype(&self, owner: Entity) -> Option<&Archetype> {
-        self.archetypes.get(owner.id())?.as_ref()
+        let (alive, archetype) = self.entities.get(owner.id())?;
+        if *alive { Some(archetype) } else { None }
     }
 
     #[must_use]
     pub fn archetype_mut(&mut self, owner: Entity) -> Option<&mut Archetype> {
-        self.archetypes.get_mut(owner.id())?.as_mut()
+        let (alive, archetype) = self.entities.get_mut(owner.id())?;
+        if *alive { Some(archetype) } else { None }
     }
 
     pub fn destroy(&mut self, entity: Entity) {
-        let Some(element) = self.archetypes.get_mut(entity.id()) else {
+        let Some((alive, _)) = self.entities.get_mut(entity.id()) else {
             return;
         };
-        if element.is_some() {
-            self.destroyed.push(entity);
-            *element = None;
+        if *alive {
+            *alive = false;
+            self.destroyed.push(entity.id());
         }
     }
 }
@@ -57,41 +60,54 @@ impl Default for EntityManager {
 mod tests {
     use super::*;
 
+    fn dirty_archetype() -> Archetype {
+        let mut archetype = Archetype::new();
+        archetype.add(0);
+        archetype
+    }
+
+    fn clean_archetype() -> Archetype {
+        Archetype::new()
+    }
+
     #[test]
-    fn new_spawn_destroy_archetype() {
+    fn new_spawn_destroy_archetype_archetype_mut() {
         let mut entity_manager = EntityManager::new();
         let entity1_1 = entity_manager.spawn();
-        assert!(entity_manager.archetype(entity1_1).is_some());
+        *entity_manager.archetype_mut(entity1_1).unwrap() = dirty_archetype();
         let entity2_1 = entity_manager.spawn();
-        assert!(entity_manager.archetype(entity2_1).is_some());
+        *entity_manager.archetype_mut(entity2_1).unwrap() = dirty_archetype();
         entity_manager.destroy(entity1_1);
         assert!(entity_manager.archetype(entity1_1).is_none());
         entity_manager.destroy(entity2_1);
         assert!(entity_manager.archetype(entity2_1).is_none());
         let entity2_2 = entity_manager.spawn();
         assert!(entity2_2 == entity2_1);
-        assert!(entity_manager.archetype(entity2_2).is_some());
+        assert!(
+            *entity_manager.archetype_mut(entity2_2).unwrap()
+                == clean_archetype()
+        );
         let entity1_2 = entity_manager.spawn();
         assert!(entity1_2 == entity1_1);
-        assert!(entity_manager.archetype(entity1_2).is_some());
-    }
-
-    #[test]
-    fn new_spawn_destroy_archetype_mut() {
-        let mut entity_manager = EntityManager::new();
+        assert!(
+            *entity_manager.archetype_mut(entity1_2).unwrap()
+                == clean_archetype()
+        );
+        entity_manager.destroy(entity2_2);
+        assert!(entity_manager.archetype(entity2_2).is_none());
+        entity_manager.destroy(entity1_2);
+        assert!(entity_manager.archetype(entity1_2).is_none());
         let entity1_1 = entity_manager.spawn();
-        assert!(entity_manager.archetype_mut(entity1_1).is_some());
+        assert!(entity1_1 == entity1_2);
+        assert!(
+            *entity_manager.archetype_mut(entity1_1).unwrap()
+                == clean_archetype()
+        );
         let entity2_1 = entity_manager.spawn();
-        assert!(entity_manager.archetype_mut(entity2_1).is_some());
-        entity_manager.destroy(entity1_1);
-        assert!(entity_manager.archetype_mut(entity1_1).is_none());
-        entity_manager.destroy(entity2_1);
-        assert!(entity_manager.archetype_mut(entity2_1).is_none());
-        let entity1_2 = entity_manager.spawn();
-        assert!(entity1_2 != entity1_1);
-        assert!(entity_manager.archetype_mut(entity1_2).is_some());
-        let entity2_2 = entity_manager.spawn();
-        assert!(entity2_2 != entity2_1);
-        assert!(entity_manager.archetype_mut(entity2_2).is_some());
+        assert!(entity2_1 == entity2_2);
+        assert!(
+            *entity_manager.archetype_mut(entity2_1).unwrap()
+                == clean_archetype()
+        );
     }
 }
